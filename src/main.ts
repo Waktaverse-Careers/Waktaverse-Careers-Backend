f240616cd37ca3aa344804b014f91de508e87ca5
iqeq1945 <iqeq1945@gmail.com>
2024-08-29 13:06:43 +0900

    feat : template clear

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // cors origin true
+  app.enableCors();
+
+  // Validator Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Waktaverse Carrer')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('Waktaverse-Carrer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
